82bf872eb73c6a918b6e44bb8d8481929d929cd2
Anastasia <149483716+nasteap2015@users.noreply.github.com>
2025-01-16 19:22:13 +0100

    Initialize image-optimizer

---
+++ vite.config.js
@@ -0,0 +1,56 @@
+import { defineConfig } from 'vite';
+// import glob from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { globalStylesOptions } from './global.styles';
+
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
+    root: 'src',
+    // build: {
+    //   sourcemap: true,
+
+    //   rollupOptions: {
+    //     input: glob.sync('./src/*.html'),
+    //     output: {
+    //       manualChunks(id) {
+    //         if (id.includes('node_modules')) {
+    //           return 'vendor';
+    //         }
+    //       },
+    //       entryFileNames: 'commonHelpers.js',
+    //     },
+    //   },
+    //   outDir: '../dist',
+    // },
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/**.html']),
+      ViteImageOptimizer({
+        png: {
+          // https://sharp.pixelplumbing.com/api-output#png
+          quality: 60,
+        },
+        jpeg: {
+          // https://sharp.pixelplumbing.com/api-output#jpeg
+          quality: 60,
+        },
+        jpg: {
+          // https://sharp.pixelplumbing.com/api-output#jpeg
+          quality: 60,
+        },
+      }),
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: globalStylesOptions,
+        },
+      },
+    },
+  };
+});
