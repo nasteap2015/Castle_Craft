2b57aa36636714eb8cd0460b04d36bd5cfd17011
Alla Sopinska <sopinskaya10475@gmail.com>
2025-01-20 21:06:04 +0200

    update header.js

--- src/js/mobile-nemu.js
+++ src/js/mobile-nemu.js
@@ -1,8 +1,10 @@
-const burgerBtn = document.querySelector('.burger-menu');
+const openBtn = document.querySelector('.burger-menu');
 const closeBtn = document.querySelector('.close-btn');
 const backdropMenu = document.querySelector('.backdrop');
+const allSite = document.querySelector('body');
+const menuLinks = document.querySelectorAll('.mob-nav-link');
 
-burgerBtn.addEventListener('click', e => {
+openBtn.addEventListener('click', e => {
   openMobileMenu();
 });
 
@@ -13,10 +15,25 @@ closeBtn.addEventListener('click', e => {
 function openMobileMenu(e) {
   backdropMenu.style.display = 'block';
   setTimeout(() => {
+    allSite.classList.add('noscroll');
     backdropMenu.classList.add('is-open');
   }, 300);
+
+  closeBtn.addEventListener('click', closeMobileMenu);
+  menuLinks.forEach(menuLink => {
+    menuLink.addEventListener('click', closeMobileMenu);
+  });
 }
 
 function closeMobileMenu(e) {
+  allSite.classList.remove('noscroll');
   backdropMenu.classList.remove('is-open');
+  setTimeout(() => {
+    backdropMenu.style.display = 'none';
+  }, 1000);
+
+  closeBtn.removeEventListener('click', closeMobileMenu);
+  menuLinks.forEach(menuLink => {
+    menuLink.removeEventListener('click', closeMobileMenu);
+  });
 }
